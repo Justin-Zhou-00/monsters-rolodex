c9c7c8fda62bf4511b8d3aaefad8daa661459f5d
Justin Zhou <justinzhou68@gmail.com>
2020-02-03 18:57:13 +1300

    Updates

---
+++ precache-manifest.1fdcbc2e5e65ab720ca7d0b610b45945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24659d45d4034614b578e70eec63587",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d137652c49f48839e395",
+    "url": "/monsters-rolodex/static/css/main.5585bb7a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d137652c49f48839e395",
+    "url": "/monsters-rolodex/static/js/main.d96caece.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
